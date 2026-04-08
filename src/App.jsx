cbd56d44106c7906aee340d865465bffdaa945c4
dishank choudhury <dishankchoudhury@gmail.com>
2026-04-08 23:04:05 +0530

    app.jsx file there

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WallCalendar from "./components/calendar/WallCalendar";
+
+function App() {
+  return (
+    <div className="app-container">
+      <WallCalendar />
+    </div>
+  );
+}
+
+export default App;
